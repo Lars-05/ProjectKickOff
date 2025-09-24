8a16920790cc039c3caef3ac3153ba9491fcc0ad
Lars Versteegen <lars.versteegen6@gmail.com>
2025-09-24 22:09:59 +0200

    Added Buddy Saving

--- Assets/SpawnBuddy.cs
+++ Assets/SpawnBuddy.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnBuddy : MonoBehaviour
@@ -6,19 +7,26 @@ public class SpawnBuddy : MonoBehaviour
     [SerializeField] private GameObject spawnPoint; 
     [SerializeField] private GameObject buddyPrefab;
     [SerializeField] private GameObject objectToSpawnUnder;
-
+    [HideInInspector] public List<int> buddyIds = new List<int>();
     public void Awake()
     { 
-        SaveData saveData = SaveAndLoadSystem.LoadData(); 
-        if(saveData == null)
+      
+        if(SaveAndLoadSystem.LoadData() == null)
             return;
+        SaveData saveData = SaveAndLoadSystem.LoadData();
+
+        foreach (var ID in saveData.buddyData.buddyIds)
+        {
+           SpawnBuddyWithID(ID);
+        }
+        
     }
     
     public void SpawnBuddyWithID(int buddyID)
     { 
-        Debug.Log("ffffffff");
         GameObject newBuddyPrefab = Instantiate(buddyPrefab, spawnPoint.transform.position, Quaternion.identity, objectToSpawnUnder.transform);
         SpriteRenderer buddySpriteRenderer = newBuddyPrefab.GetComponentInChildren<SpriteRenderer>();
         buddySpriteRenderer.sprite = buddieSprites[buddyID];
+        buddyIds.Add(buddyID);
     }
 }
